149adf398f0296c73d64e92c09e93da259fe97a0
Mahmoud Al-baba <131158371+mahmod98albaba@users.noreply.github.com>
2023-11-29 20:56:42 +0300

    Delete any directory

--- any/README.md
+++
@@ -1,2 +0,0 @@
-"# addtocart" 
-"# addtocart" 
--- any/src/any/Parameters.java
+++
@@ -1,9 +0,0 @@
-package any;
-
-public class Parameters {
-
-	static String Url = "https://www.saucedemo.com/";
-	static String UserName = "standard_user";
-	static String Pass = "secret_sauce";
-	
-}
